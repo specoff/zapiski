0be7f7020b93e820ca147b1fe967757283539fdd
specoff <specoff@yandex.ru>
2013-10-03 16:18:48 +0300

    Snippets etc.
    html, php, css, js

---
+++ zapiski.php
@@ -0,0 +1,19 @@
+<html>
+
+//Автоотступ работает для тех элементов которые идут в потоке.
+
+#Table_01 {
+        position: relative;
+        margin: 0 auto;
+        width: 960px;
+}
+
+
+// В <table class="tbl-col"> отступы вокруг ячеек и столбцов
+
+.tbl-col{
+border-collapse: separate;
+border-spacing: 14px;
+}
+
+</html>
